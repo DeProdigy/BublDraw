6fa8e38939768780c04071562c60925686c3f0a0
DeProdigy <alex.hint.ah@gmail.com>
2013-11-28 16:12:48 -0500

    set up a global current color, fixing up the hover and click events

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
-  createCubes();
-  addEventListeners();
+  createcircles();
   addColorPalette();
+  addEventListeners();
+  window.colorChosen = "palette-1";
 });
 
-function createCubes(){
+function createcircles(){
   var $body = $("body");
   var $canvas = $("<div class='canvas'></div>");
   //#of rows
@@ -17,7 +18,7 @@ function createCubes(){
         //how big the squares are
         .css("width", 20)
         .css("height", 20)
-        .addClass("cubes");
+        .addClass("circles");
       $row.append($cube);
     }
   }
@@ -26,32 +27,51 @@ function createCubes(){
 
 function addEventListeners(){
   //hovers
-  $(".cubes").mouseover(function() {
-    $(this).addClass('hoveredGreen');
-  });
-  $(".cubes").mouseout(function() {
-    $(this).removeClass('hoveredGreen');
-  });
+  //user hover on and off the circle
+  $(".circles").on('mouseover', hoveredOn);
+  $(".circles").on('mouseout', hoveredOff);
 
   //paints
-  $(".cubes").mousedown(function() {
-    $(this).addClass('clickedGreen');
-      $(".cubes").on("mouseover", paintCLicked);
-  $(".cubes").mouseup(function() {
-    $(".cubes").off("mouseover", paintCLicked);
+  //user clicks down
+  $(".circles").on('mousedown', function() {
+    //shut off hovers
+    $(".circles").off('mouseover', hoveredOn);
+    $(".circles").off('mouseout', hoveredOff);
+    //add that color to the circle that was clicked
+    $(this).addClass(window.colorChosen);
+    //start painting
+    $(".circles").on("mouseover", paintCLicked);
+    //user releases click, stop painting
+    $(".circles").mouseup(function() {
+      $(".circles").off("mouseover", paintCLicked);
+    });
+
   });
+
+  //select color
+  $(".colors").on("click", function(){
+    var color = this.id;
+    window.colorChosen = color;
   });
 }
 
+function hoveredOn() {
+  $(this).addClass(window.colorChosen);
+}
+
+function hoveredOff() {
+  $(this).removeClass(window.colorChosen);
+}
+
 function paintCLicked() {
-  $(this).addClass('clickedGreen');
+  $(this).addClass(window.colorChosen);
 }
 
 function addColorPalette() {
   var colorArray = ["#ECF0F1", "#2ECC71", "#3498DB", "#F1C40F", "#E74C3C", "#2C3E50"];
-  var $colors = $("<div>").addClass("colors");
+  var $colors = $("<div>").addClass("colorPalette");
   for (var i = 0; i < colorArray.length; i++) {
-    $colors.append('<div style="background-color:' +colorArray[i]+'" id="palette-' + i + '" class="colorPalette"></div>');
+    $colors.append('<div style="background-color:' +colorArray[i]+'" id="palette-' + i + '" class="colors"></div>');
   }
   $('body').append($colors);
 }
