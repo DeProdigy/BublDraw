0cded8d7f6cdfbb0da1b8d1b608a1c1e2ec7f11c
DeProdigy <alex.hint.ah@gmail.com>
2013-11-28 14:20:51 -0500

    fist commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bubldraw::Application.config.secret_token = '7d481bf39302ab20363cbdda6f8eb3b8b8adba16d174ceab3db28bdba71211356428a57e231717f9156c8f623cdfc4ba598d85ef254ae918a56f3f1ad2db9633'
